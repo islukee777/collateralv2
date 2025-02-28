a76ed6bc621ce1ac4d05afaf3b043ee2f99f65cb
islukee777 <166670945+islukee777@users.noreply.github.com>
2025-02-28 18:11:56 +1100

    last twiter commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,22 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import React, { useEffect } from 'react';
+import App from './App'; // Removed .tsx extension
+import './index.css';
+
+// Dynamically add Google Fonts link to the document head
+const addGoogleFonts = () => {
+  const link = document.createElement('link');
+  link.href = 'https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@400;700&family=Atkinson+Hyperlegible:wght@400;700&family=Bangers&family=Borel&family=Cherry+Cream+Soda&family=Chewy&family=Edu+VIC+WA+NT+Beginner:wght@400;700&family=Fugaz+One&family=Indie+Flower&family=Noto+Sans+Warang+Citi&family=Poiret+One&family=Short+Stack&family=Sour+Gummy:ital,wght@0,400;1,400&display=swap';
+  link.rel = 'stylesheet';
+  document.head.appendChild(link);
+};
+
+const Main = () => {
+  useEffect(() => {
+    addGoogleFonts();
+  }, []);
+
+  return <App />;
+};
 
 createRoot(document.getElementById("root")!).render(<App />);
